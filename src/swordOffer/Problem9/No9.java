908a0bd8101108f12fd1a856e7040387a0f35a48
YHGui <yhgui2014@gmail.com>
2018-12-07 20:32:41 +0800

    sword to offer No.9

---
+++ src/swordOffer/Problem9/No9.java
@@ -0,0 +1,49 @@
+package swordOffer.Problem9;
+
+public class No9 {
+    public static void main(String[] args) {
+        System.out.println(fibRecur(5));
+    }
+
+    private static long fibRecur(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("the param is less than 0");
+        }
+
+        if (0 == n) {
+            return 0;
+        }
+
+        if (1 == n) {
+            return 1;
+        }
+
+        return fibRecur(n - 1) + fibRecur(n - 2);
+    }
+
+    private static long fib(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("the param is less than 0");
+        }
+
+        if (0 == n) {
+            return 0;
+        }
+
+        if (1 == n) {
+            return 1;
+        }
+
+        int fib1 = 0;
+        int fib2 = 1;
+        int fibN = 0;
+
+        for (int i = 2; i <= n; i++) {
+            fibN = fib1 + fib2;
+            fib1 = fib2;
+            fib2 = fibN;
+        }
+
+        return fibN;
+    }
+}
