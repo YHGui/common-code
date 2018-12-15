c6d372d6bd644623bd69a1c7b92007993223a8c3
YHGui <yhgui2014@gmail.com>
2018-12-15 10:34:41 +0800

    sword offer No.16 & test case

--- src/swordOffer/Problem16/No16.java
+++ src/swordOffer/Problem16/No16.java
@@ -4,7 +4,22 @@ import swordOffer.Problem5.ListNode;
 
 public class No16 {
     public static void main(String[] args) {
+        ListNode a = new ListNode("1");
+        ListNode b = new ListNode("2");
+        ListNode c = new ListNode("3");
+        ListNode d = new ListNode("4");
+        ListNode e = new ListNode("5");
+        ListNode f = new ListNode("6");
+        ListNode g = new ListNode("7");
+        a.setNext(b);
+        b.setNext(c);
+        c.setNext(d);
+        d.setNext(e);
+        e.setNext(f);
+        f.setNext(g);
 
+        ListNode mid = getMid(a);
+        System.out.println(mid.getData());
     }
 
     private static ListNode getMid(ListNode head) {
