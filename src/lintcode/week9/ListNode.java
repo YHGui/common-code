ddbcda179737a2f12929c093fd6764a72a37c597
YHGui <yhgui2014@gmail.com>
2018-12-15 11:49:47 +0800

    sword offer No.17 & test case

--- src/lintcode/week9/ListNode.java
+++ src/lintcode/week9/ListNode.java
@@ -4,9 +4,9 @@ package lintcode.week9;
  * Created by Katsura on 2017/4/8.
  */
 public class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int val) {
+    public int val;
+    public ListNode next;
+    public ListNode(int val) {
         this.val = val;
         this.next = null;
     }
---
+++ src/swordOffer/Problem17/No17.java
@@ -0,0 +1,58 @@
+package swordOffer.Problem17;
+
+import lintcode.week9.ListNode;
+
+public class No17 {
+    public static void main(String[] args) {
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(13);
+        ListNode c = new ListNode(5);
+        ListNode d = new ListNode(7);
+        ListNode e = new ListNode(2);
+        ListNode f = new ListNode(5);
+        ListNode g = new ListNode(3);
+        ListNode h = new ListNode(9);
+        a.next = c;
+        c.next = d;
+        e.next = g;
+        g.next = f;
+        f.next = h;
+        h.next = b;
+
+
+        ListNode finalNode = mergeTwoLists(a, e);
+        while (finalNode != null) {
+            System.out.println(finalNode.val);
+            finalNode = finalNode.next;
+        }
+
+    }
+
+    private static ListNode mergeTwoLists(ListNode left, ListNode right) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                tail.next = left;
+                tail = left;
+                left = left.next;
+            } else {
+                tail.next = right;
+                tail = right;
+                right = right.next;
+            }
+
+        }
+
+        if (left != null) {
+            tail.next = left;
+        }
+
+        if (right != null) {
+            tail.next = right;
+        }
+
+        return dummy.next;
+    }
+}
